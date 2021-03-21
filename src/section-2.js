52321a0b721261b044d2f353502c7c6022de04d9
luckystar20206 <luckystar20206@gmail.com>
2021-03-21 17:59:31 +0530

    initial

--- src/section-2.js
+++ src/section-2.js
@@ -6,18 +6,18 @@ const section = () => {
 		<div className="row-a">
 			<div className="col-a">
 				<div className="col-a1">
-					<span className='tt'>Increase your business</span>
-					 <span className='tt-1'>traffic</span>
+					<span className="tt">Increase your business</span>
+					<span className="tt-1">traffic</span>
 				</div>
 				<div className="col-a2">
 					We are committed to processing the information in order to contact you and talk about your project.
 				</div>
-                <div className="col-a3">
-                    <h3 class='ttt'>LEARN MORE</h3>
-                </div>
+				<div className="col-a3">
+					<h3 class="ttt">LEARN MORE</h3>
+				</div>
 			</div>
 			<div className="col-b">
-				<img src={image} alt="image" />
+				<img src={image} alt="imag" />
 			</div>
 		</div>
 	);
